cc699478226e518e810083ac74f6e087c1b79877
Chase Almy <chase@chases-air.mynetworksettings.com>
2023-07-01 09:36:42 -0400

    selection check

--- code.ts
+++ code.ts
@@ -1,14 +1,19 @@
 const selection = figma.currentPage.selection
 
-for (let node of selection) {
-  if (node.type === 'FRAME'){
-    const children = node.children
-    for (let child of children) {
-      if ('characters' in child) {
-        if (!node.name.includes(child.characters)){
-          node.name = node.name + (node.name[-1] === '_' ? '': '_') + child.characters
+if (selection.length === 0){
+  figma.notify('nothing selected')
+}
+else {
+  for (let node of selection) {
+    if (node.type === 'FRAME'){
+      const children = node.children
+      for (let child of children) {
+        if ('characters' in child) {
+          if (!node.name.includes(child.characters)){
+            node.name = node.name + (node.name[-1] === '_' ? '': '_') + child.characters
+          }
+          break
         }
-        break
       }
     }
   }
